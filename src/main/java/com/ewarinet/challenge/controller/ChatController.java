2be59bf31c4751959d00101dcbd5ab1e7e4d69d0
Esteban <ewarinet@glamit.com.ar>
2022-03-10 20:19:14 -0300

    main -> Primer commit del codigo creado

---
+++ src/main/java/com/ewarinet/challenge/controller/ChatController.java
@@ -0,0 +1,28 @@
+package com.ewarinet.challenge.controller;
+
+import com.ewarinet.challenge.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat/send-message")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat/add-user")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+}
